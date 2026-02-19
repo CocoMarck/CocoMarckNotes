ac4a6dd03c9a9090de2816d25643d7210eea723c
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-02-18 20:45:02 -0600

    shell scripts

---
+++ bash/execute-sat-certifica-64-bits.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod ./jre1.8.0_471/bin/java -jar
+./jre1.8.0_471/bin/java -jar ./Certifica.jar
---
+++ bash/see-tree.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dir="$(dirname "$(realpath "$0")")"
+
+cd $dir
+
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
