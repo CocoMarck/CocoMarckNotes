5ded34bcc36be8dc88590e89bb422b5777f017f3
Jean Abraham <jeanabrahamchc@gmail.com>
2026-05-21 16:50:30 -0600

    script

--- calculix/code/example-01-ccx2paraview.py
+++ calculix/code/example-01-ccx2paraview.py
@@ -1,5 +1,7 @@
 '''
 Ejecutar como `py -3.11`
+
+py -3.11 ".\example-01-cc2paraview.py" --ccx "%USERPROFILE%\Build-CalculiX-2.20.0-win-x64\bin\ccx.exe"
 '''
 # Python
 import os, subprocess, random, pathlib, shutil
@@ -10,6 +12,12 @@ from ccx2paraview import Converter
 from frd2vtu import frd2vtu
 import multiprocessing
 
+# Input
+import argparse
+parser = argparse.ArgumentParser(description="CCX tool example for ParaView")
+parser.add_argument("--ccx", required=True, help="ccx.exe solver file")
+args = parser.parse_args()
+
 
 # Directorios y archivos
 cwd = os.getcwd()
@@ -19,12 +27,13 @@ desktop_echo_output = subprocess.check_output(
 
 CWD_DIR = pathlib.Path(cwd)
 WORK_DIR = pathlib.Path( desktop_echo_output )
+INPS_DIR = WORK_DIR.joinpath("paraview-inp-files")
 INPUT_FRD_DIR = WORK_DIR.joinpath("paraview-input-frd-files")
 OUTPUT_VTU_DIR = WORK_DIR.joinpath("paraview-output-vtu-files")
 
-WORK_DIRS = [CWD_DIR, WORK_DIR, INPUT_FRD_DIR, OUTPUT_VTU_DIR]
+WORK_DIRS = [CWD_DIR, WORK_DIR, INPS_DIR, INPUT_FRD_DIR, OUTPUT_VTU_DIR]
 
-CCX_EXECUTABLE_FILE = None
+CCX_EXECUTABLE_FILE = args.ccx
 
 DICT_WORK_DIRS = {}
 
@@ -44,6 +53,8 @@ if DICT_WORK_DIRS["Desktop"]["exists"]:
         os.mkdir( DICT_WORK_DIRS["paraview-input-frd-files"]["path"] )
     if not DICT_WORK_DIRS["paraview-output-vtu-files"]["exists"]:
         os.mkdir( DICT_WORK_DIRS["paraview-output-vtu-files"]["path"] )
+    if not DICT_WORK_DIRS["paraview-inp-files"]["exists"]:
+        os.mkdir( DICT_WORK_DIRS["paraview-inp-files"]["path"] )
 print()
 
 
@@ -65,8 +76,9 @@ def get_recursive_tree(path: object | pathlib.Path ) -> dict:
 
 
 # Process `inp` with `ccx`
-def ccx_process_inp(path):
-    pass 
+def ccx_process_inp(inp_file):
+    subprocess.run([CCX_EXECUTABLE_FILE, str(inp_file).replace(inp_file.suffix,"")])
+    return True
 
 # Funcs Converter
 ridiculous_size_in_bytes = 20
@@ -100,6 +112,39 @@ def move_vtu_files(input_dir, output_dir):
     except Exception as e:
         print(e)
         return False
+
+def move_frd_files(input_dir, output_dir):
+    tree = get_recursive_tree(input_dir)
+    try:
+        for f in tree["files"]:
+            if f.suffix.lower() in [".dat", ".sta", ".cvg", ".12d", ".frd"]:
+                target = output_dir.joinpath(f.name)
+                if target.exists():
+                    target.unlink()
+                shutil.move( str(f), str(output_dir) )
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def process_inp_files(input_dir, output_dir):
+    tree_of_dir = get_recursive_tree(input_dir)
+    inp_files = []
+    for f in tree_of_dir["files"]:
+        if f.suffix == ".inp":
+            inp_files.append(f)
+
+    if len(inp_files) == 0:
+        print("Nothing bro...")
+        return
+    
+    try:
+        for i in inp_files:
+            ccx_process_inp(i)
+        move_frd_files(input_dir, output_dir)
+    except Exception as e:
+        print(e)
 
 def convert_all_things(converter, input_dir, output_dir):
     '''
@@ -139,8 +184,13 @@ def convert_all_things(converter, input_dir, output_dir):
             print(e)
             #traceback.print_exc() # Error completo
         print()
-print("# Converting files")
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
+    print("# Process inp files")
+    process_inp_files(INPS_DIR, INPUT_FRD_DIR)
+    print()
+    
+    print("# Converting files")
     convert_all_things("ccx2paraview", INPUT_FRD_DIR, OUTPUT_VTU_DIR)
+    #convert_all_things("frd2vtu", INPUT_FRD_DIR, OUTPUT_VTU_DIR)
