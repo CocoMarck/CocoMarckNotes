c8fa2f8ff64f98ae3f175af1c708a44057af1c98
Jean Abraham <jeanabrahamchc@gmail.com>
2026-06-19 10:39:31 -0600

    es url

--- batch/keyboard_shortcuts_in_windows_url.bat
+++ batch/keyboard_shortcuts_in_windows_url.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start "" "https://support.microsoft.com/en-us/windows/keyboard-shortcuts-in-windows-dcc61a57-8ff0-cffe-9796-cb9706c75eec"
+start "" "https://support.microsoft.com/es-es/windows/keyboard-shortcuts-in-windows-dcc61a57-8ff0-cffe-9796-cb9706c75eec"
