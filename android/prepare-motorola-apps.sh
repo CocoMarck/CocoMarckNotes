3b1b1e463e369a719099182972e51544880bc439
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-07-03 22:05:41 -0600

    Apps

--- android/prepare-motorola-apps.sh
+++ android/prepare-motorola-apps.sh
@@ -6,7 +6,6 @@ instruction_get_apps=$1
 # Borrar feos
 $instruction_get_apps "adb shell pm uninstall -k --user 0" ./android-remove-apps-motorola.txt
 $instruction_get_apps "adb shell pm uninstall -k --user 0" ./android-google-trash.txt
-$instruction_get_apps "adb shell pm uninstall -k --user 0" ./android-google-internet-apps.txt
 
 # Asegurar chidos
 $instruction_get_apps "adb shell cmd package install-existing" ./android-play-store-dependencies.txt
