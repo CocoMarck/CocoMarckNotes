2bbb7fac201479376a5251c6836fa357a8d249ec
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-07-03 21:57:48 -0600

    better

--- bash/instruction-get-apps.sh
+++ bash/instruction-get-apps.sh
@@ -3,17 +3,28 @@
 instruction="$1"
 shift
 
+# Directorio real del script
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
 # Aplicaciones seleccionadas
 apps=()
 
 for file in "$@"; do
-    apps+=($(./get-apps.sh "$file"))
+    apps+=($("$SCRIPT_DIR/get-apps.sh" "$file"))
 done
 
 
 for app in "${apps[@]}"; do
+    echo $instruction $app
     $instruction $app
+    echo
 done
 
 
-# USO: ./instruction-get-apps.sh "adb shell pm uninstall -k --user 0 " ../../package-manager-backup/android/android-remove-apps-lenovo.txt
+# USOS:
+# ./instruction-get-apps.sh "adb shell pm uninstall -k --user 0 " ../../package-manager-backup/android/android-remove-apps-motorola.txt
+# ./instruction-get-apps.sh "adb shell pm uninstall -k --user 0 " ../../package-manager-backup/android/android-google-trash.txt
+# ./instruction-get-apps.sh "adb shell pm uninstall -k --user 0 " ../../package-manager-backup/android/android-google-internet-apps.txt
+# ./instruction-get-apps.sh "adb shell cmd package install-existing" ../../package-manager-backup/android/android-play-store-dependencies.txt
+# ./instruction-get-apps.sh "adb shell cmd package install-existing" ../../package-manager-backup/android/android-google-internet-apps.txt
+
